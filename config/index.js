99cf13327edbcd9ff94b08f4cbd84f2d6c7e579a
Roman Didukh <newrdlink@yandex.ru>
2021-03-14 18:00:51 +0300

    some fix

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@ const { NODE_ENV, JWT_SECRET } = process.env;
 
 const production = () => NODE_ENV === 'production';
 
-const JWT_WORD = production() ? JWT_SECRET : 'The time has come';
+const JWT_WORD = production() ? JWT_SECRET : 'The time has';
 // const BD_ADD = production() ? BD_ADDRESS : 'mongodb://localhost:27017/newsexplorerbd';
 const BD_ADD = 'mongodb://localhost:27017/vstoronabd';
 
