bdb629b68d9f3adaa633138e27adef9eaeb44b2d
Roman Didukh <newrdlink@yandex.ru>
2021-06-20 16:22:17 +0300

    upload file worker to server ok

--- controllers/workers.js
+++ controllers/workers.js
@@ -4,8 +4,6 @@ const Worker = require('../models/worker');
 // const pathToPublic = require('../config');
 const AlreadyExists = require('../errors/already-exists');
 const { alreadyExists } = require('../constants/errorMessages');
-
-const tempPathToPublic = './public/workers';
 const cutExpStr = require('../helpers/cutExpansionFile');
 
 const getWorkers = (req, res, next) => {
@@ -15,7 +13,6 @@ const getWorkers = (req, res, next) => {
 };
 
 const createWorker = async (req, res, next) => {
-  console.log(__dirname);
   const workerInfo = JSON.parse(req.body.workerInfo);
   const sampleFile = req.files.myfile;
   const dirFileName = cutExpStr(sampleFile.name);
@@ -30,14 +27,8 @@ const createWorker = async (req, res, next) => {
     // image,
   } = workerInfo;
   // make path where file will create
-  const dirPath = path.join(__dirname,'..','public','workers', dirFileName);
+  const dirPath = path.join(__dirname, '..', 'public', 'workers', dirFileName);
   const uploadPath = path.normalize(path.join(dirPath, fileName));
-  //
-  const pathToFileForBD = path.join(dirPath, fileName);
-  console.log(1, pathToFileForBD);
-  console.log(dirPath);
-  // console.log(fileName);
-  //
   // checking if there is a folder
   if (fs.existsSync(dirPath)) {
     return next(new AlreadyExists(alreadyExists.dirFounded));
@@ -58,14 +49,10 @@ const createWorker = async (req, res, next) => {
           position,
           image: `http://api.vstorona.didrom.ru/workers/${dirFileName}/${sampleFile.name}`,
         })
-          .then((worker) => res.send({ message: worker.image }))
+          .then((worker) => res.send({ message: worker.position }))
           .catch(next);
-        console.log(2);
       });
-      console.log(3);
     }
-    console.log('Папка создана');
-    //
   });
   return null;
 };
