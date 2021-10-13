0dd12cde3adf909f893f9a25e9c80ee36d544a17
Roman Didukh <newrdlink@yandex.ru>
2021-10-13 11:41:33 +0300

    add news model

--- controllers/news.js
+++ controllers/news.js
@@ -1,3 +1,5 @@
+const path = require('path');
+// const fs = require('fs');
 const News = require('../models/news');
 
 const getNews = (req, res, next) => {
@@ -9,21 +11,39 @@ const getNews = (req, res, next) => {
 
 const createNews = (req, res, next) => {
   // console.log('1');
+
+  const newsData = JSON.parse(req.body.newsData);
+  const imagesFront = req.files.imageFilesNews;
+  // console.log(newsData);
+
   const {
     title,
     subtitle,
-    link,
-    image,
+    description,
     createdAt,
-  } = req.body;
+  } = newsData;
 
+  const images = [];
+  const folderNameNews = createdAt.slice(0, 16).replace(':', '');
+  const dirPath = path.join(__dirname, '..', 'public/news', folderNameNews);
+
+  imagesFront.forEach((image) => {
+    const uploadPath = path.normalize(path.join(dirPath, image.name));
+    image.mv(uploadPath, (error) => {
+      if (error) { throw next(error); }
+    });
+    const pathImage = `https://api.vs.didrom.ru/news/${folderNameNews}/${image.name}`;
+    images.push(pathImage);
+  });
+
+  // console.log();
   News.create({
     title,
     subtitle,
-    link,
-    image,
-    creator: req.user.id,
+    images,
+    description,
     createdAt,
+    creator: req.user.id,
   })
     .then((news) => res.send({ id: news.id }))
     .catch(next);
